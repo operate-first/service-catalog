b41e255f79fa5f3d2b8fd09931a16c17276e68cc
Tomas Coufal <tcoufal@redhat.com>
2022-09-08 16:54:48 +0200

    fix: Allow delete of orphans
    Signed-off-by: Tomas Coufal <tcoufal@redhat.com>

--- packages/backend/src/plugins/permission.ts
+++ packages/backend/src/plugins/permission.ts
@@ -3,22 +3,31 @@ import { createRouter } from '@backstage/plugin-permission-backend';
 import {
   AuthorizeResult,
   PolicyDecision,
+  isPermission,
 } from '@backstage/plugin-permission-common';
 import {
+  catalogConditions,
+  createCatalogConditionalDecision,
+} from '@backstage/plugin-catalog-backend/alpha';
+import {
   PermissionPolicy,
-  PolicyQuery
+  PolicyQuery,
 } from '@backstage/plugin-permission-node';
+import { catalogEntityDeletePermission } from '@backstage/plugin-catalog-common/alpha';
 import { Router } from 'express';
 import { PluginEnvironment } from '../types';
 
 class ReadOnlyPermissionPolicy implements PermissionPolicy {
   async handle(request: PolicyQuery): Promise<PolicyDecision> {
-    const permissions = [
-      'catalog.entity.read',
-      'catalog.entity.refresh'
-    ];
+    const permissions = ['catalog.entity.read', 'catalog.entity.refresh'];
     if (permissions.includes(request.permission.name)) {
       return { result: AuthorizeResult.ALLOW };
+    }
+    if (isPermission(request.permission, catalogEntityDeletePermission)) {
+      return createCatalogConditionalDecision(
+        request.permission,
+        catalogConditions.hasAnnotation('backstage.io/orphan'),
+      );
     }
     return { result: AuthorizeResult.DENY };
   }
