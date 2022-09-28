7b59625fc7950192d98a976ff174b996e1b7fca3
SamoKopecky <skopecky@redhat.com>
2022-09-28 13:15:36 +0200

    feat: Add cluster status client for frontend
    Signed-off-by: SamoKopecky <skopecky@redhat.com>

---
+++ packages/app/src/clusterClient.ts
@@ -0,0 +1,16 @@
+import { ConfigApi } from "@backstage/core-plugin-api";
+import { ClusterDetails } from "@internal/plugin-cluster-status-backend";
+
+export const getClusters = async (configApi: ConfigApi): Promise<ClusterDetails[]> => {
+  return clusterApiFetchCall(configApi, '')
+}
+
+export const getClusterByName = async (configApi: ConfigApi, name: string): Promise<ClusterDetails> => {
+  return clusterApiFetchCall(configApi, `/${name}`)
+}
+
+const clusterApiFetchCall = (configApi: ConfigApi, params: string): Promise<any> => {
+  const backendUrl = configApi.getString('backend.baseUrl');
+  const jsonResponse = fetch(`${backendUrl}/api/cluster-status/status${params}`).then(r => r.json())
+  return jsonResponse;
+}
