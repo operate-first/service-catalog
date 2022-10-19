bfcaeda58def54ef883ac707c2d338bc2c4a7c23
Tomas Coufal <tcoufal@redhat.com>
2022-10-19 15:38:47 +0200

    feat: Enable metrics and healthz endpoints
    Signed-off-by: Tomas Coufal <tcoufal@redhat.com>

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -21,6 +21,8 @@ import {
 } from '@backstage/backend-common';
 import { TaskScheduler } from '@backstage/backend-tasks';
 import { Config } from '@backstage/config';
+import healthcheck from './plugins/healthcheck';
+import { metricsInit, metricsHandler } from './metrics';
 import app from './plugins/app';
 import auth from './plugins/auth';
 import catalog from './plugins/catalog';
@@ -78,12 +80,14 @@ function makeCreateEnv(config: Config) {
 }
 
 async function main() {
+  metricsInit();
   const config = await loadBackendConfig({
     argv: process.argv,
     logger: getRootLogger(),
   });
   const createEnv = makeCreateEnv(config);
 
+  const healthcheckEnv = useHotMemoize(module, () => createEnv('healthcheck'));
   const catalogEnv = useHotMemoize(module, () => createEnv('catalog'));
   const scaffolderEnv = useHotMemoize(module, () => createEnv('scaffolder'));
   const authEnv = useHotMemoize(module, () => createEnv('auth'));
@@ -113,6 +117,8 @@ async function main() {
 
   const service = createServiceBuilder(module)
     .loadConfig(config)
+    .addRouter('', await healthcheck(healthcheckEnv))
+    .addRouter('', metricsHandler())
     .addRouter('/api', apiRouter)
     .addRouter('', await app(appEnv));
 
