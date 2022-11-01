8f688fca83272e67809ac69fcce572731236bb3d
Tom Coufal <7453394+tumido@users.noreply.github.com>
2022-11-01 13:24:05 +0100

    feat: Create catalog provider for ACM (#164)
    * feat: Create catalog provider for ACM
    
    Signed-off-by: Tomas Coufal <tcoufal@redhat.com>
    
    * feat: Create custom preprocessor to add OCP logo
    
    Signed-off-by: Tomas Coufal <tcoufal@redhat.com>
    
    Signed-off-by: Tomas Coufal <tcoufal@redhat.com>

--- packages/backend/src/plugins/catalog.ts
+++ packages/backend/src/plugins/catalog.ts
@@ -1,14 +1,66 @@
-import { CatalogBuilder } from '@backstage/plugin-catalog-backend';
+import { Entity, ResourceEntity } from '@backstage/catalog-model';
+import {
+  CatalogBuilder,
+  CatalogProcessor,
+  CatalogProcessorCache,
+  CatalogProcessorEmit,
+} from '@backstage/plugin-catalog-backend';
+import { LocationSpec } from '@backstage/plugin-catalog-common';
 import { ScaffolderEntitiesProcessor } from '@backstage/plugin-scaffolder-backend';
+import { ManagedClusterProvider } from '@internal/backstage-plugin-rhacm-backend';
 import { Router } from 'express';
 import { PluginEnvironment } from '../types';
+
+class OpenshiftResourceProcessor implements CatalogProcessor {
+  getProcessorName(): string {
+    return 'OpenshiftResourceProcessor';
+  }
+
+  async preProcessEntity(
+    entity: Entity,
+    _location: LocationSpec,
+    _emit: CatalogProcessorEmit,
+    originLocation: LocationSpec,
+    _cache: CatalogProcessorCache,
+  ): Promise<Entity> {
+    if (entity.kind !== 'Resource') {
+      return entity;
+    }
+    const resource = entity as ResourceEntity;
+    if (resource.spec.type !== 'kubernetes-cluster') {
+      return resource;
+    }
+
+    if (originLocation.type === 'rhacm-managed-cluster') {
+      resource.metadata.annotations ||= {};
+      resource.metadata.annotations['operate-first.cloud/logo-url'] =
+        'https://upload.wikimedia.org/wikipedia/commons/3/3a/OpenShift-LogoType.svg';
+    }
+    return resource;
+  }
+}
 
 export default async function createPlugin(
   env: PluginEnvironment,
 ): Promise<Router> {
   const builder = await CatalogBuilder.create(env);
+  const rhacm = ManagedClusterProvider.fromConfig(env.config, {
+    logger: env.logger,
+  });
   builder.addProcessor(new ScaffolderEntitiesProcessor());
+  builder.addEntityProvider(rhacm);
+  builder.addProcessor(new OpenshiftResourceProcessor());
   const { processingEngine, router } = await builder.build();
   await processingEngine.start();
+
+  await env.scheduler.scheduleTask({
+    id: 'run_rhacm_refresh',
+    fn: async () => {
+      await rhacm.run();
+    },
+    frequency: { minutes: 30 },
+    timeout: { minutes: 10 },
+  });
+
   return router;
 }
