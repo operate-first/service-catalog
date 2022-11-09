ac1dd767c7340d332a97a5ea7bb4cf0b321e75c5
Tomas Coufal <tcoufal@redhat.com>
2022-11-09 18:58:25 +0100

    feat: Create LinkTiles component
    Signed-off-by: Tomas Coufal <tcoufal@redhat.com>

---
+++ packages/app/src/components/LinkTiles/LinkTile.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { EntityLink } from '@backstage/catalog-model';
+import { Link } from '@backstage/core-components';
+import { useApp } from '@backstage/core-plugin-api';
+import { BackstageTheme } from '@backstage/theme';
+import { Box, createStyles, makeStyles, Typography } from '@material-ui/core';
+import LanguageIcon from '@material-ui/icons/Language';
+import ArrowRightIcon from '@material-ui/icons/ArrowForwardIos';
+
+const useStyles = makeStyles((theme: BackstageTheme) =>
+  createStyles({
+    card: {
+      border: `1px solid ${theme.palette.divider}`,
+      boxShadow: theme.shadows[2],
+      borderRadius: '4px',
+      gap: '1em',
+      padding: theme.spacing(2),
+      color: '#fff',
+      transition: `${theme.transitions.duration.standard}ms`,
+      '&:hover': {
+        boxShadow: theme.shadows[4],
+      },
+      background: theme.page.backgroundImage,
+    },
+    link: {
+      '&:hover': {
+        textDecoration: 'none',
+      }
+    }
+  }),
+);
+
+const LinkTile = ({url, title, icon }: EntityLink) => {
+  const classes = useStyles();
+  const app = useApp();
+
+  const Icon = icon ? app.getSystemIcon(icon) ?? LanguageIcon : LanguageIcon;
+
+  return (
+    <Link to={url} variant="body2" className={classes.link}>
+      <Box
+        className={classes.card}
+        display="flex"
+        alignItems="center"
+      >
+        <Icon />
+        <Typography variant="h6">
+          {title}
+        </Typography>
+        <ArrowRightIcon />
+      </Box>
+    </Link>
+  )
+}
+
+export default LinkTile;
---
+++ packages/app/src/components/LinkTiles/LinkTiles.tsx
@@ -0,0 +1,22 @@
+import { HorizontalScrollGrid } from '@backstage/core-components';
+import { useEntity } from '@backstage/plugin-catalog-react';
+import { Grid } from '@material-ui/core';
+import React from 'react';
+import LinkTile from './LinkTile';
+
+const LinkTiles = () => {
+  const { entity } = useEntity();
+
+  if (!entity.metadata.links) {
+    return null;
+  }
+
+  return (
+    <HorizontalScrollGrid>
+      { entity.metadata.links.map(l => <Grid item><LinkTile {...l}/></Grid>) }
+    </HorizontalScrollGrid>
+  )
+
+}
+
+export default LinkTiles;
