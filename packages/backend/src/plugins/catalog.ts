6198a22b6747cfa59bcfe8d2fbd69ef71a126719
Michal Drla <mdrla@redhat.com>
2023-02-27 11:49:28 +0100

    fix: Fix originLocation.type check and prometheus.io/alert annotation (#219)
    * fix: Fix originLocation.type check
    
    Signed-off-by: Michal Drla <mdrla@redhat.com>
    
    * fix: typo in prometheus.io/alert annotation
    
    Signed-off-by: Michal Drla <mdrla@redhat.com>
    
    ---------
    
    Signed-off-by: Michal Drla <mdrla@redhat.com>

--- packages/backend/src/plugins/catalog.ts
+++ packages/backend/src/plugins/catalog.ts
@@ -32,10 +32,10 @@ class OpenshiftResourceProcessor implements CatalogProcessor {
       return resource;
     }
 
-    if (originLocation.type === 'rhacm-managed-cluster') {
+    if (originLocation.type === 'ocm-managed-cluster') {
       resource.metadata.annotations ||= {};
       const clusterId = resource.metadata.annotations[ANNOTATION_CLUSTER_ID];
-      resource.metadata.annotations['prometheus.io/alerts'] = 'all';
+      resource.metadata.annotations['prometheus.io/alert'] = 'all';
       resource.metadata.annotations[
         'prometheus.io/labels'
       ] = `managed_cluster_id=${clusterId}`;
